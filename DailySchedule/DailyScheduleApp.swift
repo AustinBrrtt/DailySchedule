4e74d292a61e1ef337b1eb1200a38b5f8755ac8b
Austin Barrett <austinbrrtt@gmail.com>
2020-07-18 19:31:53 -0400

    Initial Commit

---
+++ DailySchedule/DailyScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScheduleApp.swift
+//  DailySchedule
+//
+//  Created by Austin Barrett on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct DailyScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
